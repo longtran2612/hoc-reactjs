af8f264cbf38c2d3f1a0790270eff7371af8d159
Your Name <email@example.com>
2022-01-17 10:59:35 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
 import './App.css';
 import{useState} from 'react';
-
+import Content from './components/content';
 function App() {
 
-
+const [show, setshow] = useState(false)
   
-  const [job, setjob] = useState('')
-  const [jobs, setjobs] = useState(
-    JSON.parse(localStorage.getItem('jobs')) ?? []
-  )
-
-  const onAdd=()=>{
-    setjobs(prev=>{
-    const newjobs = [...prev,job]
-    const jobJSON=  JSON.stringify(newjobs)
-    localStorage.setItem('jobs',jobJSON)
-    return newjobs
-    })
-    setjob("")
-  }
-  const onDelete=(i)=>{
-   setjobs(prev=>{
-     const newjobs= [...prev];
-     newjobs.splice(i,1)
-
-     const jobJSON=  JSON.stringify(newjobs)
-     localStorage.setItem('jobs',jobJSON)
-
-     return newjobs;
-   })
-
-  }
+  
   return (
     <div className="App" style={{padding :20}}>
-     <input value={job} onChange={e=>setjob(e.target.value)}/>
-     <button onClick={onAdd}>add</button>
-     <ul>
-       {jobs.map((job,i) => (
-        // <div >
-         <li key={i} >{job}
-         <button onClick={()=>onDelete(i)}>xoa</button>
-         </li>
-         
-        // </div>
-       ))}
-       
-     </ul>
+      <button onClick={()=>setshow(!show)}>show</button>
+      {show && <Content/> }
     </div>
   );
 }
---
+++ src/components/content.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Content() {
+    return (
+      
+            <h1>day la the contents</h1>
+     
+    )
+}
