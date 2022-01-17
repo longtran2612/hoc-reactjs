2b7b0d662f1664facbc4e49557644a53ed165cd4
Your Name <email@example.com>
2022-01-17 11:56:57 +0700

    call api

--- src/components/content.js
+++ src/components/content.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { useEffect,useState } from 'react'
 
+//useEffect(callback)
+// goi callback sau khi moi khi component re-render
+//
+//useEffect(callback,[])
+// chi goi callback 1 lan sau khi mount
+//useEffect(callback,[dependentces])
+//-- callback goi lai khi deps thay doi
+
+//1. call back luon luon goi sau khi component mounted
+const tabs =['posts','photos','albums']
 export default function Content() {
+    const [posts, setposts] = useState([])
+    const [style, setstyle] = useState('posts')
+
+    //useEffect(callback,[dependentces])
+    useEffect(()=>{
+        console.log('title changed')
+            fetch(`https://jsonplaceholder.typicode.com/${style}`)
+            .then(res=>res.json())
+            .then(posts=>{
+                setposts(posts)
+            })
+    },[style])
+    
+    
+    
+
     return (
-      
-            <h1>day la the contents</h1>
-     
+      <div>
+          {
+              tabs.map(tab=>(
+                  <button key={tab}
+                  style={style===tab ?{color:'ActiveBorder'}:{}}
+                  onClick={()=>setstyle(tab)}
+                  >{tab}</button>
+              ))
+          }
+
+       <ul>
+           {posts.map(post=>(
+           <li key={post.id}>{post.title}</li>
+           ))}
+       </ul>
+
+      </div>
+          
     )
 }
