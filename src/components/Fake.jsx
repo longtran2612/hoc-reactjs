8523181932490db0610c7a8eca158e5bae01874e
Your Name <email@example.com>
2022-01-18 22:18:25 +0700

    useReducer

--- src/components/Fake.jsx
+++ src/components/Fake.jsx
@@ -1,62 +1,41 @@
 import React from 'react'
-import { useMemo,useState,useLayoutEffect,useRef,useCallback } from 'react'
+import { useReducer,useState,useLayoutEffect,useRef,useCallback } from 'react'
 
-//usememoo
-//tranh rerender 
-//
+//useReducer
+// init state
+// action:
+// reducer
+// dispatch : kick hoat 1 acction
 
-export default function Fake() {
-    const [name, setname] = useState('')
-    const [price, setprice] = useState('')
-    const [products, setproducts] = useState([])
-
-    const nameref = useRef()
-
-    const handleSubmit=()=>{
-        setproducts([...products,{
-            name,
-            price : +price
-        }])
-        setname('')
-        setprice('')
-
-        nameref.current.focus()
-
+// init 0
+const initState = 0
+// actions
+const UP_ACTION ='up'
+const DOWN_ACTION='down'
+//reducer
+const reducer =(state,action)=>{
+    console.log('reducer running');
+    switch(action){
+        case UP_ACTION:
+            return state+1
+        case DOWN_ACTION:
+            return state-1
+        default:
+            throw new console.error('inlalid');
     }
 
-    const total = useMemo(() => {
-        const result = products.reduce((result,product)=>{
-            console.log('tinhh');
-            return result +product.price
-        },0)
+}
 
-        return result
-    }, [products])
-    
-
+export default function Fake() {
+    const [count, dispatch] = useReducer(reducer,initState)
+   
 
 
     return (
         <div style={{padding :20}}>
-            <input ref={nameref} value={name} onChange={e=>setname(e.target.value)} placeholder='enter name'/>
-
-            <input value={price} onChange={e=>setprice(e.target.value)} placeholder='enter price'/>
-
-            <br/>
-
-            <button onClick={handleSubmit}>submit</button>
-
-            <br/>
-            total:{total}
-            <ul>
-                {
-                    products.map((product,i)=>(
-                        <li key={i}>{product.name}-{product.price}</li>
-                    )
-                    )
-                }
-            </ul>
-
+          <h1>{count}</h1>
+          <button onClick={()=>dispatch(UP_ACTION)}> up</button>
+          <button onClick={()=>dispatch(DOWN_ACTION)}> down</button>
         </div>
     )
 }
